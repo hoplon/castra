a1e823c7813fab24a8320671b150353afc3858cc
Micha Niskin <asdf@ubergibson.com>
2013-06-06 20:44:06 -0400

    exceptions

--- src/wigwam_clj/exception.clj
+++ src/wigwam_clj/exception.clj
@@ -5,20 +5,22 @@
 (defmulti ex-severity identity)
 
 (defmacro defex
-  [type code & [severity message]]
-  (let [ex?    (isa? code ::exception)
-        class* (if ex? code ::exception)
-        code*  (if ex? (ex-status code) code)
-        msg*   (or message (if ex? (ex-message code) "Internal server error."))
-        svr*   (or severity (if ex? (ex-severity code) :error))]
+  [type code & more]
+  (let [message   (first (filter string? more))
+        severity  (first (filter keyword? more))
+        ex?       (isa? code ::exception)
+        class*    (if ex? code ::exception)
+        code*     (if ex? (ex-status code) code)
+        msg*      (or message (if ex? (ex-message code) "Internal server error."))
+        svr*      (or severity (if ex? (ex-severity code) :error))]
     `(do
        (derive ~type ~class*)
        (defmethod ex-status ~type [_#] ~code*)
        (defmethod ex-message ~type [_#] ~msg*)
        (defmethod ex-severity ~type [_#] ~svr*))))
 
-(defex ::csrf     403 :error "There was a problem. Are cookies disabled?")
-(defex ::auth     403 :error "Please log in to continue.")
+(defex ::csrf     403 "There was a problem. Are cookies disabled?")
+(defex ::auth     403 "Please log in to continue.")
 (defex ::ignore   500 :ignore)
 (defex ::debug    500 :debug)
 (defex ::info     500 :info)
@@ -29,8 +31,11 @@
 
 (defn ex
   "Create new wigwam exception."
-  [type & [message data cause]]
-  (let [m (or message (ex-message type))
+  [type & more]
+  (let [message (first (filter string? more))
+        data    (first (filter map? more))
+        cause   (first (filter (partial instance? Throwable) more))
+        m (or message (ex-message type))
         d {:type      type
            :data      data}]
     (ex-info m d cause)))
@@ -38,7 +43,7 @@
 (defn ex->clj
   "Get exception properties and data as a clj map."
   [e]
-  (let [e (if (isa? (:type (ex-data e)) ::exception) e (ex ::fatal nil nil e))
+  (let [e (if (isa? (:type (ex-data e)) ::exception) e (ex ::fatal e))
         p (ex-data e)
         t (:type p)
         a (ancestors t)
@@ -49,6 +54,10 @@
     {:type t :isa a :message m :severity s :data d :cause c}))
 
 (comment
-  ;; inheritance: use code and severity of fatal ex
-  (defex ::more-fatal ::fatal nil "A most serious error happened.")
+
+  ;; inheritance: use code and severity of fatal ex, but not message
+  (defex ::more-fatal ::fatal "A most serious error happened.")
+
+  ;; use code and message of fatal ex, but not severity
+  (defex ::less-fatal ::fatal :warning)
   )
