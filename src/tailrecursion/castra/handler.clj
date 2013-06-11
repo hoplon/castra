23c7ba94f1045bad197ef4df29536998d3c6e7dc
Micha Niskin <asdf@ubergibson.com>
2013-06-10 21:04:23 -0400

    remove more middleware; use edn for transport

--- src/tailrecursion/castra/handler.clj
+++ src/tailrecursion/castra/handler.clj
@@ -3,6 +3,7 @@
     [ring.middleware.session.cookie   :as c]
     [ring.util.codec                  :as u :refer [url-decode base64-encode]]
     [clojure.set                      :as s :refer [intersection difference]]
+    [clojure.tools.reader.edn         :as e :refer [read-string]]
     [tailrecursion.castra.exception   :as x :refer [ex ex->clj]]
     [tailrecursion.castra.core        :as r :refer [*request* *session*]]))
 
@@ -22,22 +23,25 @@
     (apply fun args)))
 
 (defn select-vars [nsname & {:keys [only exclude]}]
-  (let [var-fn?   #(fn? (var-get %))
-        to-var    #(resolve (symbol (str nsname) (str %)))
+  (let [to-var    #(resolve (symbol (str nsname) (str %)))
         to-vars   #(->> % (map to-var) (keep identity) set)
         var-pubs  #(do (require %) (vals (ns-publics %))) 
-        vars      (->> nsname var-pubs (filter var-fn?) set)
+        vars      (->> nsname var-pubs set)
         only      (if (seq only) (to-vars only) vars)
         exclude   (if (seq exclude) (to-vars exclude) #{})]
     (-> vars (intersection only) (difference exclude))))
 
 (defn castra [& namespaces]
-  (let [seq* #(or (try (seq %) (catch Throwable e)) [%])
+  (let [head {"Content-type" "application/edn"}
+        seq* #(or (try (seq %) (catch Throwable e)) [%])
+        tagx #(let [x (ex->clj %)] (with-meta x {::status (:status x)}))
         vars (->> namespaces (map seq*) (mapcat #(apply select-vars %)) set)]
     (fn [request]
-      (binding [*request* (atom request), *session* (atom (:session request))]
-        (let [d #(do (csrf!) (do-rpc vars (:body request))) 
-              r (try (d) (catch Throwable e e))
-              x (when (instance? Throwable r) (ex->clj r))
-              h {"X-Csrf" (:x-csrf @*session*)}]
-          {:status (:status x 200), :headers h, :body (or x r), :session @*session*})))))
+      (if-not (= :post (:request-method request))
+        {:status 404 :headers {} :body ""}
+        (binding [*request* (atom request), *session* (atom (:session request))]
+          (let [f #(do (csrf!) (do-rpc vars (read-string %)))
+                b (try (f (:body request)) (catch Throwable e (tagx e)))
+                s (::status (meta b) 200)
+                h (assoc head "X-Csrf" (:x-csrf @*session*))]
+            {:status s, :headers h, :body (pr-str b), :session @*session*}))))))
