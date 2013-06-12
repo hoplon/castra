3868d09f8a7ec64acc0fe5139715b727136904ce
Micha Niskin <asdf@ubergibson.com>
2013-06-12 11:01:39 -0400

    Ditch macros, accept vector expr for remote call

--- src/tailrecursion/castra.cljs
+++ src/tailrecursion/castra.cljs
@@ -28,16 +28,17 @@
     (if c (reset! csrf c))
     (try (read-string t) (catch js/Error e (jsex->ex e)))))
 
-(defn ajax [async? edn out err fin fails]
+(defn ajax [async? expr out err fin fails]
   (let [csrf-kw   :tailrecursion.castra/csrf
-        retry!    #(ajax async? edn out err fin (inc fails))
+        expr      (if (string? expr) expr (pr-str expr))
+        retry!    #(ajax async? expr out err fin (inc fails))
         handle-ex #(if (and (< fails 2) (isa? %2 csrf-kw)) (retry!) (%1 %2))
         wrap-out  (fn [_ _ x] (let [d (xhr! x)] ((if (ex? d) err out) d)))
         wrap-err  (fn [x _ _] (let [d (xhr! x)] (handle-ex err (make-ex d))))
         settings  {"async"        async?
                    "complete"     fin
                    "contentType"  "application/edn"
-                   "data"         edn
+                   "data"         expr
                    "dataType"     "text"
                    "error"        wrap-err 
                    "headers"      {"X-Csrf" @csrf
@@ -48,9 +49,9 @@
                    "url"          *url*}]
     (-> js/jQuery (.ajax (clj->js settings)))))
 
-(defn remote [async? edn & [out err fin]]
+(defn remote [async? expr & [out err fin]]
   (let [wrap #(or % (constantly true))]
-    (ajax async? edn (wrap out) (wrap err) (wrap fin) 0)))
+    (ajax async? expr (wrap out) (wrap err) (wrap fin) 0)))
 
 (def async (partial remote true))
 (def sync  (partial remote false))
