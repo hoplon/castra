4b05b0b2464d22656e57f258e2f2adebacf165f8
Micha Niskin <micha.niskin@gmail.com>
2014-02-09 11:38:05 -0500

    Factor out the scraping from the middleware

--- src/tailrecursion/castra/middleware.clj
+++ src/tailrecursion/castra/middleware.clj
@@ -20,30 +20,33 @@ page.open(url, function(status) {
   }, 0);
 });")
 
-(defn phantomjs [js-path url]
-  (let [{:keys [exit err out]} (sh/sh "phantomjs" js-path url)]
-    (or (and (zero? exit) out)
-        (throw (Exception. (format "phantomjs: %s (%d)" err exit))))))
-
 (defn tmp-file [ext content]
   (.getPath (doto (File/createTempFile "tmp-" ext) (spit content))))
 
+(defn scrape-phantomjs []
+  (let [js-path (tmp-file js)]
+    (fn [url]
+      (let [{:keys [exit err out]} (sh/sh "phantomjs" js-path url)]
+        (or (and (zero? exit) out)
+          (throw (Exception. (format "phantomjs: %s (%d)" err exit))))))))
+
 (defn wrap-escaped-fragment
-  "Middleware to detect Google's '_escaped_fragment_' AJAX crawling requests [1]
-  and serve a PhantomJS rendered version of the page.
+  "Middleware to detect Google's `_escaped_fragment_` AJAX crawling requests [1]
+  and serve a PhantomJS rendered version of the page. The `scrape` parameter
+  must be a function that takes a URL and returns the HTML body of the rendered
+  page (or throws an exception). See `scrape-phantomjs` above.
 
   NB: This middleware needs to be wrapped in middleware that will provide the
       :params request map key. A suitable candidate for this could be the ring
       #'ring.middleware.params/wrap-params middleware, for instance.
 
   [1]: https://developers.google.com/webmasters/ajax-crawling/docs/specification"
-  [handler]
-  (let [scrape (partial phantomjs (tmp-file js))]
-    (fn [{{frag "_escaped_fragment_"} :params
-          {:strs [host]}              :headers
-          :keys [scheme uri request-method] :as req}]
-      (let [{:keys [status] :as resp} (handler req)
-            err   {:status 500 :body "Server Error"}
-            url   (str (name scheme) "://" host uri "#!" frag)
-            resp? (not (and frag (= [200 :get] [status request-method])))]
-        (if resp? resp (try (assoc resp :body (scrape url)) (catch Throwable _ err)))))))
+  [handler scrape]
+  (fn [{{frag "_escaped_fragment_"} :params
+        {:strs [host]}              :headers
+        :keys [scheme uri request-method] :as req}]
+    (let [{:keys [status] :as resp} (handler req)
+          err   {:status 500 :body "Server Error"}
+          url   (str (name scheme) "://" host uri "#!" frag)
+          resp? (not (and frag (= [200 :get] [status request-method])))]
+      (if resp? resp (try (assoc resp :body (scrape url)) (catch Throwable _ err))))))
