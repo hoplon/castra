48057916e5917e1c65994380c231a63046058c36
Micha Niskin <micha.niskin@gmail.com>
2014-02-09 10:54:04 -0500

    Only render in phantomjs if URL would be handled otherwise

--- src/tailrecursion/castra/middleware.clj
+++ src/tailrecursion/castra/middleware.clj
@@ -31,12 +31,15 @@ page.open(url, function(status) {
   [1]: https://developers.google.com/webmasters/ajax-crawling/docs/specification"
   [handler]
   (let [js-path (.getPath (doto (File/createTempFile "scrape-" ".js") (spit js)))
-        scrape  #(let [{:keys [exit err out]} (sh/sh "phantomjs" js-path %)]
-                   (or (and (zero? exit) out)
-                       (throw (Exception. (str "status: " exit ": " err)))))]
-    (fn [{{frag "_escaped_fragment_"} :params {:strs [host]} :headers :keys [scheme uri] :as req}]
-      (if-not frag
-        (handler req)
-        (try 
-          {:status 200 :body (scrape (str (name scheme) "://" host uri "#!" frag))}
-          (catch Throwable _ {:status 500 :body "Server Error"}))))))
+        phantom #(sh/sh "phantomjs" js-path %)
+        ph-err  #(Exception. (format "phantomjs: %s (%d)" %1 %2))
+        scrape  #(let [{:keys [exit err out]} (phantom %)]
+                   (if (zero? exit) out (throw (ph-err err exit))))]
+    (fn [{{frag "_escaped_fragment_"} :params
+          {:strs [host]}              :headers
+          :keys [scheme uri request-method] :as req}]
+      (let [{:keys [status] :as resp} (handler req)
+            err   {:status 500 :body "Server Error"}
+            body  #(scrape (str (name scheme) "://" host uri "#!" frag))
+            resp? (not (and (= [200 :get] [status request-method]) frag))]
+        (if resp? resp (try (assoc resp :body (body)) (catch Throwable _ err)))))))
