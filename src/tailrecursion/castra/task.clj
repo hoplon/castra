f70b7d2021f6d1b3e5145e7def012783dbe75d30
mynomoto <mynomoto@gmail.com>
2014-04-28 10:17:20 -0300

    Add options to castra-dev-server

--- src/tailrecursion/castra/task.clj
+++ src/tailrecursion/castra/task.clj
@@ -10,7 +10,13 @@
   (r/ring-task (fn [_] (apply c/castra specs))))
 
 (core/deftask castra-dev-server
-  "Creates a server for development with castra."
-  [& namespaces]
-  (comp (r/head) (r/dev-mode) (r/session-cookie) (r/files)
-        (apply castra namespaces) (r/jetty)))
+  "Creates a server for development with castra. The first argument is
+  a quoted namespace or a quoted vector of namespaces with Castra endpoints."
+  [namespaces & {:keys [port join? key docroot]
+      :or {port    8000
+           join?   false
+           key     "a 16-byte secret"
+           docroot (core/get-env :out-path)}}]
+  (comp (r/head) (r/dev-mode) (r/session-cookie key) (r/files docroot)
+        (if (coll? namespaces) (apply castra namespaces) (castra namespaces))
+        (r/jetty :port port :join? join?)))
