aebc5c37fc8558255c1756c0f6342a5619f84cd0
Micha Niskin <micha.niskin@gmail.com>
2015-05-22 13:24:29 -0400

    Use options map instead of dynamic vars for endpoint configuration
    - New options map keys and associated types:
    
      {:ajax-fn     fn
       :json->clj   fn
       :clj->json   fn
       :url         str
       :timeout     int
       :credentials bool}

--- src/tailrecursion/castra.cljs
+++ src/tailrecursion/castra.cljs
@@ -9,29 +9,17 @@
 (ns tailrecursion.castra
   (:require [cognitect.transit :as t]))
 
-(def ^:dynamic *validate-only*
-  "Only validate request parameters, don't actually do it?"
-  nil)
-
-(def ^:dynamic *ajax-timeout*
-  "The timeout for XHR requests in ms (0 means no timeout)."
-  (atom 0))
-
-(def ^:dynamic *with-credentials*
-  "Send cookies when making CORS requests?"
-  (atom true))
-
-(def json->clj
-  "Atom containing function to convert wire format JSON to ClojureScript data."
-  (atom (partial t/read (t/reader :json))))
-
-(def clj->json
-  "Atom containing function to convert ClojureScript data to wire format JSON."
-  (atom (partial t/write (t/writer :json))))
+;; helpers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- safe-pop
   [x]
   (or (try (pop x) (catch js/Error e)) x))
+
+;; public api ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def ^:dynamic *validate-only*
+  "Only validate request parameters, don't actually do it?"
+  nil)
 
 (defn ex?
   "Returns true if x is an ExceptionInfo."
@@ -50,57 +38,53 @@
         (aset "serverStack" stack)
         (aset "status" status)))))
 
-(defn json->clj*
-  "Converts wire format JSON to CLJS data (see json->clj above). If an
-  exception is thrown during conversion an ExceptionInfo object is returned."
-  [x]
-  (try (@json->clj x)
-       (catch js/Error e
-         (make-ex {:message "Server Error" :cause e}))))
-
-(def ajax-fn
-  "Atom containing the ajax request implementation. The default is to use the
-  standard jQuery ajax machinery."
-  (atom (fn [url data headers done fail always]
-          (let [opts {"async"       true
-                      "contentType" "application/json"
-                      "data"        data
-                      "dataType"    "text"
-                      "headers"     headers
-                      "processData" false
-                      "type"        "POST"
-                      "url"         url
-                      "timeout"     *ajax-timeout*
-                      "xhrFields"   {"withCredentials" @*with-credentials*}}]
-            (-> js/jQuery
-                (.ajax (clj->js opts))
-                (.done (fn [_ _ x] (done (json->clj* (aget x "responseText")))))
-                (.fail (fn [x t r] (let [status  (.-status x)
-                                         head    (aget x "getResponseHeader")
-                                         tunnel? (.call head x "X-Castra-Tunnel")
-                                         body    (if-not tunnel?
-                                                   {:message r :status status}
-                                                   (json->clj* (aget x "responseText")))]
-                                     (fail (make-ex body)))))
-                (.always (fn [_ _] (always))))))))
+(defn ajax-fn
+  "Ajax request implementation using the standard jQuery ajax machinery."
+  [{:keys [url json->clj timeout credentials]} data headers done fail always]
+  (let [opts {"async"       true
+              "contentType" "application/json"
+              "data"        data
+              "dataType"    "text"
+              "headers"     headers
+              "processData" false
+              "type"        "POST"
+              "url"         url
+              "timeout"     timeout
+              "xhrFields"   {"withCredentials" credentials}}
+        ex    #(make-ex {:message "Server Error" :cause %})]
+    (-> js/jQuery
+        (.ajax (clj->js opts))
+        (.done (fn [_ _ x]
+                 (done (try (json->clj (aget x "responseText"))
+                            (catch js/Error e (ex e))))))
+        (.fail (fn [x t r]
+                 (let [status  (.-status x)
+                       head    (aget x "getResponseHeader")
+                       tunnel? (.call head x "X-Castra-Tunnel")
+                       body    (if-not tunnel?
+                                 {:message r :status status}
+                                 (try (json->clj (aget x "responseText"))
+                                      (catch js/Error e (ex e))))]
+                   (fail (make-ex body)))))
+        (.always (fn [_ _] (always))))))
 
 (defn- ajax
-  [url expr done fail always]
+  [{:keys [ajax-fn clj->json] :as opts} expr done fail always]
   (let [headers {"X-Castra-Csrf"          "true"
                  "X-Castra-Tunnel"        "transit"
                  "X-Castra-Validate-Only" (str (boolean *validate-only*))
                  "Accept"                 "application/json"}
-        expr    (if (string? expr) expr (@clj->json expr))
+        expr    (if (string? expr) expr (clj->json expr))
         done    #((if (ex? %) fail done) %)]
-    (ajax-fn url expr headers done fail always)))
+    (ajax-fn opts expr headers done fail always)))
 
-(defn default-opts
-  [opts]
-  (->> opts (merge {:ajax-fn     ajax-fn
-                    :json->clj   json->clj
-                    :clj->json   clj->json
-                    :timeout     timeout
-                    :credentials credentials
+(defn with-default-opts
+  [& [opts]]
+  (->> opts (merge {:timeout     0
+                    :credentials true
+                    :ajax-fn     ajax-fn
+                    :json->clj   (partial t/read (t/reader :json))
+                    :clj->json   (partial t/write (t/writer :json))
                     :url         (.. js/window -location -href)})))
 
 (defn mkremote
@@ -108,14 +92,13 @@
   optional :url keyword argument can be used to specify the URL to which the
   POST requests will be made."
   [endpoint state error loading & [opts]]
-  (let [url (or url (.. js/window -location -href))]
-    (fn [& args]
-      (let [p (.Deferred js/jQuery)]
-        (swap! loading (fnil conj []) p)
-        (let [xhr (ajax
-                    url
-                    `[~endpoint ~@args]
-                    #(do (reset! error nil) (reset! state %) (.resolve p %))
-                    #(do (reset! error %) (.reject p %))
-                    #(swap! loading (fn [x] (vec (remove (partial = p) x)))))]
-          (doto p (aset "xhr" xhr)))))))
+  (fn [& args]
+    (let [p (.Deferred js/jQuery)]
+      (swap! loading (fnil conj []) p)
+      (let [xhr (ajax
+                  (with-default-opts opts)
+                  `[~endpoint ~@args]
+                  #(do (reset! error nil) (reset! state %) (.resolve p %))
+                  #(do (reset! error %) (.reject p %))
+                  #(swap! loading (fn [x] (vec (remove (partial = p) x)))))]
+        (doto p (aset "xhr" xhr))))))
